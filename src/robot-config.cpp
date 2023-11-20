0bb0285df693b9751c653f90dc9e2c59a6c4e302
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2023-11-20 09:54:31 -0800

    Initial commit

---
+++ src/robot-config.cpp
@@ -0,0 +1,71 @@
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of brain used for printing to the V5 brain screen
+brain Brain;
+
+// Drivetrain
+motor leftMotor = motor(PORT1, ratio18_1, false);
+motor rightMotor = motor(PORT2, ratio18_1, true);
+
+inertial robotInertial = inertial(PORT10);
+
+smartdrive robotDrive = smartdrive(leftMotor, rightMotor, robotInertial, 319.9, 320, 40, mm, 1);
+
+// Arm
+motor arm = motor(PORT3, ratio18_1, false);
+
+// Intake
+motor intake = motor(PORT4, ratio18_1, false);
+
+// Controller
+controller mainController = controller(primary);
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ *
+ * This should be called at the start of your int main function.
+ */
+
+void vexcodeInit(void) {
+  // Start robot setup (initalization)
+  Brain.Screen.print("Initalizing Robot...");
+  Brain.Screen.newLine();
+
+  // Calibrate inertial sensor for drivetrain
+  Brain.Screen.newLine();
+  Brain.Screen.print("Calibrating Interial Sensor...");
+  robotInertial.calibrate();
+  // Budget thread pause
+  while (robotInertial.isCalibrating()) {
+    // Wait
+  }
+  Brain.Screen.newLine();
+  Brain.Screen.print("Inertial Sensor Calibrated");
+  Brain.Screen.newLine();
+
+  // Set drivetrain profiles
+  Brain.Screen.newLine();
+  Brain.Screen.print("Setting Drivetrain Profiles...");
+  robotDrive.setDriveVelocity(100, percent);
+  robotDrive.setTurnVelocity(50, percent);
+  Brain.Screen.newLine();
+  Brain.Screen.print("Drivetrain Ready");
+  Brain.Screen.newLine();
+
+  // Set motor profiles
+  Brain.Screen.newLine();
+  Brain.Screen.print("Setting Motor Profiles...");
+  arm.setVelocity(100, percent);
+  intake.setVelocity(100, percent);
+  Brain.Screen.newLine();
+  Brain.Screen.print("Motors Ready");
+  Brain.Screen.newLine();
+
+  // Display completed state
+  Brain.Screen.newLine();
+  Brain.Screen.print("Robot Ready");
+
+}
