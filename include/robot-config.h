190c1ae5a4f388c3395a59cc8916ed7f49b57389
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2023-11-23 10:28:26 -0800

    Include robot objects for outside robot-confing

--- include/robot-config.h
+++ include/robot-config.h
@@ -1,6 +1,12 @@
 using namespace vex;
 
+// Include brain for main.cpp
 extern brain Brain;
+// Include drivetrain for main.cpp
+extern smartdrive robotDrive;
+// Include other motors for main.cpp
+extern motor arm;
+extern motor intake;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
